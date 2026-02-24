94122c6c3515c06828d0506acbe63a2dd43baccc
Lorenzo Maffioli <lorenzo.maffioli@gmail.com>
2026-02-24 20:45:36 +0100

    wip

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	ifaceName string = "wlan0"
+	ifaceName string = "eth0"
 )
 
 func main() {
@@ -24,16 +24,16 @@ func main() {
 	frameSize := 4096
 	blockSize := frameSize * 128
 	blockTimeout := 1 * time.Millisecond
-	numBocks := 64
-	poolTimeout := 50 * time.Microsecond
+	numBlocks := 64
+	pollTimeout := 50 * time.Microsecond
 
 	handle, err := afpacket.NewTPacket(
 		afpacket.OptInterface(ifaceName),
 		afpacket.OptFrameSize(frameSize),
 		afpacket.OptBlockSize(blockSize),
 		afpacket.OptBlockTimeout(blockTimeout),
-		afpacket.OptNumBlocks(numBocks),
-		afpacket.OptPollTimeout(poolTimeout),
+		afpacket.OptNumBlocks(numBlocks),
+		afpacket.OptPollTimeout(pollTimeout),
 	)
 	if err != nil {
 		log.Fatalf("failed to create a new tpacket: %v", err)
@@ -44,36 +44,46 @@ func main() {
 	source.DecodeOptions = gopacket.Lazy
 
 	for packet := range source.Packets() {
-		timestamp := packet.Metadata().Timestamp
-
-		var srcIp, dstIp, proto string
-		var srcPort, dstPort uint16
+		timestamp := packet.Metadata().Timestamp.Format("15:04:05.000000")
 
 		if ipLayer := packet.Layer(layers.LayerTypeIPv4); ipLayer != nil {
 			ip := ipLayer.(*layers.IPv4)
-			srcIp, dstIp = ip.SrcIP.String(), ip.DstIP.String()
+			srcIP, dstIP := ip.SrcIP.String(), ip.DstIP.String()
 		}
 
 		if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
 			tcp := tcpLayer.(*layers.TCP)
-			srcPort, dstPort = uint16(tcp.SrcPort), uint16(tcp.DstPort)
-			proto = "TCP"
-		} else if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
-			udp := udpLayer.(*layers.UDP)
-			srcPort, dstPort = uint16(udp.SrcPort), uint16(udp.DstPort)
-			proto = "UDP"
-		} else if packet.Layer(layers.LayerTypeICMPv4) != nil {
-			proto = "ICMP"
-		} else {
-			fmt.Printf("Unknown packet\n")
+			srcPort, dstPort := uint16(tcp.SrcPort), uint16(tcp.DstPort)
+			fmt.Printf("%s %s:%d > %s:%d TCP\n", timestamp, srcIP, srcPort, dstIP, dstPort)
 			continue
 		}
 
-		fmt.Printf("%s %s:%d > %s:%d %s\n",
-			timestamp.Format("12:01:05.000000"),
-			srcIp, srcPort,
-			dstIp, dstPort,
-			proto,
-		)
+		if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+			udp := udpLayer.(*layers.UDP)
+			srcPort, dstPort := uint16(udp.SrcPort), uint16(udp.DstPort)
+			fmt.Printf("%s %s:%d > %s:%d UDP\n", timestamp, srcIP, srcPort, dstIP, dstPort)
+
+			if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
+				dns := dnsLayer.(*layers.DNS)
+				for _, q := range dns.Questions {
+					fmt.Printf("    DNS query:  %s (%v)\n", q.Name, q.Type)
+				}
+				for _, a := range dns.Answers {
+					fmt.Printf("    DNS answer: %s -> %s\n", a.Name, a.IP)
+				}
+			}
+			continue
+		}
+
+		if packet.Layer(layers.LayerTypeICMPv4) != nil {
+			fmt.Printf("%s %s > %s ICMP\n", timestamp, srcIP, dstIP)
+			continue
+		}
+
+		if arpL := packet.Layer(layers.LayerTypeARP); arpL != nil {
+			arp := arpL.(*layers.ARP)
+			fmt.Printf("%s ARP %v asks for %v\n", timestamp, net.IP(arp.SourceProtAddress), net.IP(arp.DstProtAddress))
+			continue
+		}
 	}
 }
